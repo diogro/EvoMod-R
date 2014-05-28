b51985a9600b6d6bcf40a4457bfc126b7d4169b3
Diogo Melo <diogro@gmail.com>
2014-05-28 11:30:24 +0100

    Rs

--- R/CalcCorrOmega.R
+++ R/CalcCorrOmega.R
@@ -20,3 +20,15 @@ CalcCorrOmegaKrz <- function(mat.list, omega){
   corr.omega <- lapply(mat.list, function(x) KrzCor(x, omega))
   return(unlist(corr.omega))
 }
+
+#'Caclulate Omega Correlation
+#'
+#'Calculates the fitness surface correlation with a list of matrices.
+#'@export
+CalcCorrOmegaRS <- function(mat.list, omega){
+  n.traits = dim(mat.list[[1]])[1]
+  omega = as.matrix(read.table ("input/omega.csv", header=F, sep=' '))[1:n.traits, 1:n.traits]
+  corr.omega <- lapply(mat.list, function(x) RandomSkewers(x, omega)[1])
+  return(unlist(corr.omega))
+}
+
