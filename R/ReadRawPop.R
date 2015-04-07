bd4e0f1054fca4d4e9133c3455b6459012450396
diogro <diogro@gmail.com>
2015-04-07 14:14:07 -0300

    raw sim read

--- R/ReadRawPop.R
+++ R/ReadRawPop.R
@@ -12,10 +12,10 @@ ReadRawPop <- function(folder = "/home/diogro/projects/evomod/c-gsl/output/burn_
   names(rawPop) <- list(paste0(c("loci_1-", "loci_2-"), rep(1:m, each = 2)),
                         c("y", paste0("trait", 1:p)),
                         paste0("ind", 1:Ne))
-                      
+  x <- (apply(rawPop, 3, function(x) t(x[,-1]) %*% x[,1]))          
   pop = list(y = rawPop[,1,], 
              B = rawPop[,-1,], 
-             x = (apply(rawPop, 3, function(x) t(x[,-1]) %*% x[,1])), 
+             x = x, 
              G = cov(x), 
              folder = folder, 
              Ne = Ne, 
