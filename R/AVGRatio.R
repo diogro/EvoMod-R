1c280e0edc774016521fd30140f86dc7dbda60ec
diogro <diogro@gmail.com>
2020-10-22 10:32:48 -0300

    man

--- R/AVGRatio.R
+++ R/AVGRatio.R
@@ -2,24 +2,15 @@
 #'
 #'Calculate the AVG ratio for a list of matrices, returning a data.frame
 #'@export
-AVGRatio <- function(mat.list, Selection_Strength, last.gen = T, num.cores = 1, generations = 1:10000 + 20000){
-  if (num.cores > 1) {
-    library(doMC)
-    library(foreach)
-    registerDoMC(num.cores)
-    parallel = TRUE
-  }
-  else{
-    parallel = FALSE
-  }
+AVGRatio <- function(mat.list, Selection_Strength, last.gen = T, generations = 1:10000 + 20000){
   n.traits = dim(mat.list[[1]])[1]
   module.1 = rep(c(1,0), each = n.traits/2)
   module.2 =  rep(c(0,1), each = n.traits/2)
   modularity.hipot = cbind(module.1, module.2)
   if(last.gen)
-    AVGRatio <- ldply(mat.list[length(mat.list)], function(x) TestModularity(x, modularity.hipot, iterations=0), .parallel = parallel)
+    AVGRatio <- ldply(mat.list[length(mat.list)], function(x) TestModularity(x, modularity.hipot))
   else{
-    AVGRatio <- ldply(mat.list, function(x) TestModularity(x, modularity.hipot, iterations=0), .parallel = parallel)
+    AVGRatio <- ldply(mat.list, function(x) TestModularity(x, modularity.hipot))
     AVGRatio['generation'] = rep(generations, each = 3)
   }
   AVGRatio['Selection_Strength'] = Selection_Strength
--- R/AVGRatioPlot.R
+++ R/AVGRatioPlot.R
@@ -2,23 +2,24 @@
 #'
 #'Plots AVGRatio output
 #'@export
-#'@import ggplot2 reshape2 plyr
-AVGRatioPlot  <- function(pop.list, modules = FALSE, num.cores = 2, x.axis = 'selection.strength', x.label = 'Peak Movement Rate'){
-  avg <- ldply(pop.list, function(x) AVGRatio(x$p.cor, x[[x.axis]], num.cores = num.cores), .progress = 'text')
-  names(avg)[6] = "Avg_Ratio"
+#'@import ggplot2 reshape2 plyr dplyr
+AVGRatioPlot  <- function(pop.list, modules = FALSE, x.axis = 'selection.strength', x.label = 'Peak Movement Rate'){
+  avg <- ldply(pop.list, function(x) AVGRatio(x$p.cor, x[[x.axis]]), .progress = 'text')
   if(modules){
-    m.avg = melt(avg[,-c(2, 3, 6)], id.vars = c('.id', 'Selection_Strength'))
-    m.avg = m.avg[!((m.avg['.id'] != "Full Integration") & (m.avg['variable'] == "AVG-")),]
-    m.avg = m.avg[!((m.avg['.id'] == "Full Integration") & (m.avg['variable'] == "AVG+")),]
-    m.avg = ddply(m.avg, .(Selection_Strength, .id, variable), 
+    m.avg = melt(avg[,-c(1, 3, 4, 7)], id.vars = c('hypothesis', 'Selection_Strength'))
+    m.avg = filter(m.avg, (hypothesis == "Full Integration" & variable == "AVG-") |
+                          (hypothesis == "module.1"         & variable == "AVG+") |
+                          (hypothesis == "module.2"         & variable == "AVG+"))
+    
+    m.avg = ddply(m.avg, .(Selection_Strength, hypothesis, variable), 
           function(x) {y <- x$value 
                        c(mean = mean(y), quantile(y, c(0.025, 0.975)))
           })
-    names(m.avg) = c('Selection_Strength', '.id', 'variable', 'mean', 'ymin', 'ymax')
+    names(m.avg) = c('Selection_Strength', 'hypothesis', 'variable', 'mean', 'ymin', 'ymax')
     avg.plot = ggplot(m.avg, aes(Selection_Strength,
                                  mean,
-                                 group=interaction(variable, Selection_Strength, .id),
-                                 colour=interaction(.id, variable))) +
+                                 group=interaction(variable, Selection_Strength, hypothesis),
+                                 colour=interaction(hypothesis, variable))) +
       geom_point() + geom_errorbar(aes(ymax = ymax, ymin = ymin)) +
       labs(x=x.label,
            y="Average Correlation",
@@ -28,7 +29,7 @@ AVGRatioPlot  <- function(pop.list, modules = FALSE, num.cores = 2, x.axis = 'se
                                      "Between Modules")) + theme_bw()
   }
   else{
-    avg.full = avg[avg['.id'] == "Full Integration",-3]
+    avg.full = avg[avg['hipothesis'] == "Full Integration",-3]
     avg.full = ddply(avg.full, .(Selection_Strength), 
                      function(x) {y <- x$Avg_Ratio 
                                   c(mean = mean(y), quantile(y, c(0.025, 0.975)))
